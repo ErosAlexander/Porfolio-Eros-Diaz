469714d11a70b5da8e8bd4f58b34615a751c3938
Eros <erosdc2018@gmail.com>
2025-03-04 21:23:19 -0300

    Añadido de secciones: PROYECTOS, PLATAFORMAS, Y INSTITUCIONES

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,9 @@ document.addEventListener('mousemove', function(event) {
     follower.style.left = `${mouseX - 6.0}px`; // Ajusta para centrar el cuadrado
     follower.style.top = `${mouseY - 6.0}px`; // Ajusta para centrar el cuadrado
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const lista = document.querySelector(".main-plataformas-elementos ul");
+    const items = lista.innerHTML; // Guarda los elementos originales
+    lista.innerHTML += items; // Duplica la lista para un desplazamiento sin cortes
+});
