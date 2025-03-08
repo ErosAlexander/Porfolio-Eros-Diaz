6c22643b4376e2e972689055ef932de6542df9d3
Eros <erosdc2018@gmail.com>
2025-03-08 17:55:02 -0300

    Proyecto ya utilizabla

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ document.addEventListener("DOMContentLoaded", () => {
     const items = lista.innerHTML; // Guarda los elementos originales
     lista.innerHTML += items; // Duplica la lista para un desplazamiento sin cortes
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const selectAsunto = document.getElementById("asunto");
+    const hiddenAsunto = document.getElementById("hidden-asunto");
+
+    selectAsunto.addEventListener("change", function () {
+        // Actualiza el campo oculto con el valor seleccionado
+        hiddenAsunto.value = selectAsunto.value;
+    });
+});
+
+document.getElementById("boton-cv").addEventListener("click", function () {
+    const link = document.createElement("a");
+    link.href = "documentos/CV_Eros_Diaz.pdf";
+    link.download = "CV_Eros_Diaz.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
